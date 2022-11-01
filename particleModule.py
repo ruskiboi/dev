04d66c5ef97924a2df88509be461212335d54204
ruskiboi <63880257+ruskiboi@users.noreply.github.com>
2022-11-01 08:54:19 -0500

    first commit

---
+++ particleModule.py
@@ -0,0 +1,43 @@
+# Solid Particles
+
+solidParticle_000 = {
+
+    "particleName": "sand",
+    "particleIndexNo": 0,
+    "particleStrength": 0.50,
+    "particleWeight": 0.25,
+    "particleDensity": 1.50,
+    "meltingPointCelsius": 600,
+    "canFloat": False,
+    "canBurn": False,
+    "canMelt": True,
+
+}
+
+solidParticle_001 = {
+
+    "particleName": "stone",
+    "particleIndexNo": 1,
+    "particleStrength": 0.75,
+    "particleWeight": 0.50,
+    "particleDensity": 2.60,
+    "meltingPointCelsius": 600,
+    "canFloat": False,
+    "canBurn": False,
+    "canMelt": True,
+
+}
+
+solidParticle_002 = {
+
+    "particleName": "brick",
+    "particleIndexNo": 1,
+    "particleStrength": 0.85,
+    "particleWeight": 0.60,
+    "particleDensity": 2.0,
+    "meltingPointCelsius": 0,
+    "canFloat": False,
+    "canBurn": False,
+    "canMelt": False,
+
+}
